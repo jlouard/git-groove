526ebeeaca592e43b369726dd27ac3a7c3577ab2
jlouard <julienl@hokla.com>
2024-11-15 12:35:30 +0100

    fix(dab): fireworks

--- utils/play_sound.sh
+++ utils/play_sound.sh
@@ -1,4 +1,10 @@
 play_sound() {
+    for arg in "$@"; do
+        if [ "$arg" = "--no-sound" ]; then
+            return 0
+        fi
+    done
+
     if [ -f "$1" ]; then
         afplay "$1" || echo "Error playing sound file: $1"
     else
